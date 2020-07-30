5c1b50fe16839896c0aa50c8169c9a376770bc33
akshay-e-k <akulkarni@chatsoft.com>
2020-07-30 22:09:27 +0530

    added job board page

--- js/dashboard-jobboard-custom.js
+++ js/dashboard-jobboard-custom.js
@@ -1,14 +1,3 @@
-// $('#togglebutton').click(function() {
-    // if ($(window).width() > 500) { //your chosen mobile res
-      // $('.text').toggle(3000);
-      // $(".menu").width(40);
-    // } else {
-    // $('.menu').animate({
-    //     width: 'toggle'
-    //   }, 350);
-    // }
-// });
-
 $(document).ready(function(){
   var initialWidth = 0;
   var initialDisplay = "";
@@ -16,7 +5,6 @@ $(document).ready(function(){
       if($(window).width() >= 768) {
         initialWidth = $(".menu").width();
         initialDisplay = $(".menu .text").css("display");
-        console.log(initialWidth, initialDisplay);
         if(initialWidth > 50) {
           initialWidth = 50;
           initialDisplay = "none";
